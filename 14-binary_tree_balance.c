296d9f2878fbb4f9e3e103d24ac9a44273d05ca7
Alem Haile <wellma0043@gmail.com>
2022-08-26 16:29:42 +0300

    did tasks

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,5 +1,29 @@
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_leaves - count the leaves in a binary tree
+ *
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: count of the number of leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	count += binary_tree_leaves(tree->left);
+	count += binary_tree_leaves(tree->right);
+
+	return (count);
+}
+
 /**
  * binary_tree_balance - measures the balance factor
  *
