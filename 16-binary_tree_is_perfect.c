c2dbe98ccb454c3276da98966ab95358e6d2148b
Alem Haile <wellma0043@gmail.com>
2022-08-27 00:02:37 +0300

    did all tasks

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,38 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_perfect - A function that checks if binary tree is perfect.
+ * @tree: A pointer to the root of the tree.
+ * Return: 1 if binary tree is perfect or 0 in otherwise.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if ((binary_tree_is_full(tree) == 1) &&
+			(binary_tree_balance(tree) == 0))
+		return (1);
+	return (0);
+}
+
+/**
+ * binary_tree_is_full - A function that checks if a binary tree is full.
+ * @tree: A pointer to the root node of the tree.
+ * Return: 1 if tree is full or 0 in otherwise.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) &&
+				binary_tree_is_full(tree->right));
+	return (0);
+}
+
+/**
  * binary_tree_balance - A function that measures the balance factor of a
  * binary tree.
  * @tree: Pointer to root node to calculate balance factor.
