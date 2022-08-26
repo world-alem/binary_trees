575944144a868c56c6b0e2fe2f7cd53e0703eecd
Alem Haile <wellma0043@gmail.com>
2022-08-26 23:21:32 +0300

    betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,11 +9,15 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	binary_tree_t *left;
+
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	if (node->parent->parent->left != NULL && node->parent->n == node->parent->parent->left->n)
+	left = node->parent->parent->left;
+
+	if (left != NULL && node->parent->n == left->n)
 		return (node->parent->parent->right);
 
-	return (node->parent->parent->left);
+	return (left);
 }
